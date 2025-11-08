4e4d71fd56675e1240cccb7f818b1b14a3550225
Andrew Nguyen <152828234+Andrew126111@users.noreply.github.com>
2025-11-08 13:05:49 -0500

    Initial commit

---
+++ my-vite-project/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import bg from './assets/pexels-pixabay-47367.jpg';
+import fbg from './assets/land.png';
+import './App.css'
+import{Parallax, ParallaxLayer } from '@react-spring/parallax';
+
+function App() {
+  return(
+    <div>
+      <Parallax pages={5}>
+        <ParallaxLayer 
+          offset={0}
+          speed={1}
+          factor={2}
+          style={{
+            backgroundImage: `url(${bg})`,
+            backgroundSize: 'cover',
+          }}
+         />
+
+        <ParallaxLayer offset={0} speed={0.05}>
+          <h2>Count</h2>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={2} speed={0.5} factor={1}>
+          <h2>My</h2>
+        </ParallaxLayer>
+        
+          <ParallaxLayer
+            offset={4}
+            speed={0.08}
+            factor={2}
+            style={{
+              backgroundImage: `url(${fbg})`,
+              backgroundSize: "cover"
+
+            }}
+          />
+
+        <ParallaxLayer offset={4} speed={0.08}>
+          <h2>Coins</h2>
+        </ParallaxLayer>
+
+      </Parallax>
+    </div>
+  )
+}
+
+export default App
