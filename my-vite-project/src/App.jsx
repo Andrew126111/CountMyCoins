dadc0275ef8d694d6de73e994b0d7d7b55959b98
Andrew Nguyen <152828234+Andrew126111@users.noreply.github.com>
2025-11-11 20:11:33 -0500

    made headings unselectable

--- my-vite-project/src/App.jsx
+++ my-vite-project/src/App.jsx
@@ -1,19 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import bg from './assets/pexels-pixabay-47367.jpg';
 import fbg from './assets/land.png';
 import coin from './assets/source.gif';
 import './App.css'
 import{Parallax, ParallaxLayer } from '@react-spring/parallax';
 import { motion } from "motion/react";
+import React, { useRef, useEffect } from 'react';
 
 
 function App() {
+  const countPage = useRef(null);
+  const myPage = useRef(null);
+  const coinsPage = useRef(null);
+  
+  const scrollToSection = () => {
+    if (targetSectionRef.current){
+      targetSectionRef.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+  }
+
   return(
     <div>
       <Parallax pages={4}>
-        <ParallaxLayer 
+        <ParallaxLayer
           offset={0}
           speed={1}
           factor={2}
@@ -23,12 +34,13 @@ function App() {
           }}
          />
 
-        <ParallaxLayer offset={0} speed={0.05}>
-          <h2>Count</h2>
+        <ParallaxLayer onScroll={scrollToSection} offset={0} speed={0.05} ref={countPage} >
+          <h2 class="unselectable">Count</h2>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={1.4} speed={0.2} factor={1}>
-          <h2>My</h2>
+        <ParallaxLayer onScroll={scrollToSection} offset={1.4} speed={0.2} factor={1} ref={myPage}>
+          <h2 class="unselectable">My</h2>
+          
         </ParallaxLayer>
 
         <ParallaxLayer
@@ -36,6 +48,7 @@ function App() {
             speed={1}
             factor={3.5}
             style={{
+              
               backgroundImage: `url(${fbg})`,
               backgroundSize: "cover",
               backgroundPosition: "center"
@@ -43,8 +56,8 @@ function App() {
             }}
         />
 
-        <ParallaxLayer offset={2.4} speed={0.05}>
-          <h2>Coins</h2>
+        <ParallaxLayer onScroll={scrollToSection} offset={2.4} speed={0.05} ref={coinsPage}>
+          <h2 class="unselectable">Coins</h2>
         </ParallaxLayer>
 
         <ParallaxLayer sticky={{start:1, end:1.4}} speed={0.05}>
@@ -52,22 +65,21 @@ function App() {
         </ParallaxLayer>
 
         <ParallaxLayer offset={3.5} speed={0.1}>
-            <importAnimate/>
+            <ImportAnimate/>
         </ParallaxLayer>
-
         
-
       </Parallax>
     </div>
   )
 }
 
-function importAnimate(){
+function ImportAnimate(){
   return(
     <div class="coinContainer">
 
     </div>
   )
 }
+
 
 export default App
